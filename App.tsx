53a3514be89abd957cbd40482345c579669c2459
Ahmed Khaled <ahmedkhaled4d@gmail.com>
2024-05-05 01:35:20 +0300

    ChatScreen

--- App.tsx
+++ App.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
+// import FlatListData from './src/screens/FlatListData';
+// import HomeScreen from './src/screens/HomeScreen';
+// import {ChatScreen} from './src/screens/ChatScreen';
+import {SafeAreaView, StyleSheet} from 'react-native';
 import FlatListData from './src/screens/FlatListData';
 
 const App = () => {
-  return <FlatListData />; // Screen 1
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatListData />
+    </SafeAreaView>
+  );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  text: {
+    fontSize: 25,
+    fontWeight: '500',
+  },
+});
 export default App;
